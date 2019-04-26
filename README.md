fab1448e5da879d5474aefba4567ca8e86152923
Coloradosunrae <stevie@ourhopecenter.org>
2019-04-26 14:26:21 -0600

    new read me

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # liri-node-app
+
+### Liri Searches Spotify for Songs, Bands in Town for concerts, and OMDB for Movies 
+
+Liri is like SIRI but instead it interprets Language. Liri is a command line node app. So npm install node modules will be essential. This uses axios for retrieving Bands in Town, Spotify, and OMDB Api's. As well as moment to format the retrieved concets. If you would like this to work you will need your own .env that has your own Spotify API key. This also ses inquirer so that it formats LIRI with questions and answers. 
+
+### What Liri Looks like to start off: 
+
+  ![Start](images/start.png)
+
+Use the down arror key to select which thing you would like to do.
+
+## concert-this 
+
+Type in the band that you would like to see. In the example I chose Bassnectar. This includes the name of the venue, venue location, and the date of the event. 
+
+  ![Concert-this](images/concert-this.png)
+
+## spotify-this-song 
+
+This asks you what song you would like to find? This retrieves a list of potential artists, a preview link of the song from spotify, and the album name from where it came. In this case I used Raindrops Keep Falling On My Head 
+
+  ![Spotify-this](images/spotify-this-song.png)
+
+    If no song is chosen it defaults to The Sign by The Ace of Base
+   ![Movie-this](images/song-this-null.png)
+
+  ## movie-this
+
+This asks you what movie you would like to see. I chose "The Grinch as an example. 
+This gives you a description of the title of the movie, the year the movie came out, the IMDB rating, the rotten tomatoes rating, the country it was produced, the language of the movie, the plot, and a list of actors from the movie.  
+
+  ![Movie-this](images/movie-this.png)
+
+  If no movie is chosen it defaults to Mr.Nobody
+   ![Movie-this](images/movie-null.png)
+
+## do-what-it-says
+
+This uses the fs Node package to read the random.txt file. In the file I currently have written spotify-this-song,"My Bad". Therefore it gives you the artist, the song name, the link from spotify, and the album name from where it came. 
+
+  ![Spotify-this](images/do-song.png)
+
+Once I change the text file to say movie-this,"Home Alone" 
+
+It will populate the movie description of "Home Alone"
+
+  ![Movie-this](images/do-what-it-says-movie.png)
+
+If you change it the .txt file to concert-this,Griz
+
+ ![Movie-this](images/do-what-it-says-concert.png)
+
--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var inquirer = require("inquirer");
 var moment = require ("moment")
+var fs = require("fs");
 
 var keys = require("./keys.js")
 
@@ -34,6 +35,8 @@ inquirer
           message: "Who do you want to see?",
           name: "artist",
         }, ]).then(function (inquirerResponse) {
+
+       
           var queryURL = "https://rest.bandsintown.com/artists/" + inquirerResponse.artist + "/events?app_id=codingbootcamp";
           axios.get(queryURL)
             .then(function (response) {
@@ -46,7 +49,7 @@ inquirer
                 )
               }
             });
-
+          
         });
 
     }
@@ -55,28 +58,40 @@ inquirer
       inquirer
         .prompt([{
           type: "input",
-          message: "What song would ou like to find?",
+          message: "What song would you like to find?",
           name: "song",
         }, ]).then(function (inquirerResponse) {
-         
+
           var spotify = new Spotify(keys.spotify);
-          
-          
-          spotify.search({ type: 'track', query: inquirerResponse.song }, function(err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
-            }
-           
-          
-          for (var i= 0; i < 5; i++) {
-              console.log (
-                  data.tracks.items[i].artists[0].name,
-                  data.tracks.items[i].name,
-                  data.tracks.items[i].preview_url,
-                  data.tracks.items[i].album.name,
-                  // JSON.stringify(data, null, 2),
-              )}
-          })
+
+          if(inquirerResponse.song === ""){
+
+            console.log ("Artist: Ace of Base")
+                console.log ("Song Title: The Sign")
+              console.log ("Spotify: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=053b32346ce84b7c9b1827653674dc55")
+              console.log ("Album: The Sign (US Album) [Remastered]")
+              console.log("----------------------------------------------------")
+            
+          }else {     
+            
+            spotify.search({ type: 'track', query: inquirerResponse.song }, function(err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+              }
+             
+            
+            for (var i= 0; i < 5; i++) {
+                console.log ("Artist: " + data.tracks.items[i].artists[0].name)
+                console.log ("Song Title: " + data.tracks.items[i].name)
+              console.log ("Spotify: " + data.tracks.items[i].preview_url)
+              console.log ("Album: " + data.tracks.items[i].album.name)
+              console.log("----------------------------------------------------")
+              
+              }
+            })
+
+          }
+         
         });
 
 
@@ -90,6 +105,11 @@ inquirer
           message: "What movie do you want to see?",
           name: "movie",
         }, ]).then(function (inquirerResponse) {
+          if(inquirerResponse.movie === ""){
+
+            console.log("Mr. Nobody came out in the year 2009, with an IMBD rating of 7.8 & a rating from Rotten Tomatoes of 67%. It was produced in the Belgium, Germany, Canada, France, USA, UK in English, Mohawk. The invigarating Plot is: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible. Starring Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham.")
+            
+          }else { 
 
        
           axios.get("http://www.omdbapi.com/?t=" + inquirerResponse.movie  + "&y=&plot=short&apikey=trilogy").then(
@@ -101,11 +121,82 @@ inquirer
               );
             }
           );
+          }
         });
 
 
 
-    } else {
+    }
+    else if (inquirerResponse.computer == "do-what-it-says") {
+
+      fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
+        }
+      
+        
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        if(dataArr[0] === "spotify-this-song"){
+          var spotify = new Spotify(keys.spotify);
+          
+          
+          spotify.search({ type: 'track', query: dataArr[1] }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          
+          for (var i= 0; i < 5; i++) {
+            console.log ("Artist: " + data.tracks.items[i].artists[0].name)
+            console.log ("Song Title: " + data.tracks.items[i].name)
+          console.log ("Spotify: " + data.tracks.items[i].preview_url)
+          console.log ("Album: " + data.tracks.items[i].album.name)
+          console.log("----------------------------------------------------")
+          }
+          })
+
+        }
+        
+        else if (dataArr[0] === "movie-this"){
+          axios.get("http://www.omdbapi.com/?t=" + dataArr[1] + "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+              // console.log(response)
+              console.log(response.data.Title + " came out in the year " + response.data.Year + ", with an IMBD rating of " + response.data.imdbRating +
+                " & a rating from " + response.data.Ratings[1].Source + " of " + response.data.Ratings[1].Value + "." + " It was produced in the " + response.data.Country +
+                " in " + response.data.Language + "." + " The invigarating Plot is: " + response.data.Plot + " Starring " + response.data.Actors + "."
+              );
+            }
+          );
+        }
+        
+        else if (dataArr[0] === "concert-this"){
+         
+          var queryURL = "https://rest.bandsintown.com/artists/" + dataArr[1] + "/events?app_id=codingbootcamp";
+          axios.get(queryURL)
+            .then(function (response) {
+              console.log("\n The concerts available are:\n");
+              for (var i = 0; i < response.data.length; i++) {
+                console.log(
+                  "The venue is " + response.data[i].venue.name +
+                  " in " + response.data[i].venue.city + " , " + response.data[i].venue.region + " on " +
+                   moment(response.data[i].datetime).format('MMMM Do YYYY, h:mm:ss a')
+                )
+              }
+            });
+        }
+        
+       
+      
+      });
+      
+
+
+
+    }  else {
       console.log("\nIt doesn't matter " + inquirerResponse.username + ", I have access anyway.\n");
     }
   });
